557b996494ff67b7aa9abafe5ca054da46156e3a
nathanielng2017 <stoneplus@berkeley.edu>
2019-11-03 15:33:02 -0800

    patch for profile for null field and added TODO for next iteration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-include DevisePermittedAttributes
+  include DevisePermittedAttributes
+  include ApplicationHelper
+
 end
---
+++ app/helpers/profile_helper.rb
@@ -0,0 +1,40 @@
+module ProfileHelper
+
+  def getProfileWithDefautCreation(userholder)
+    if not userholder.profile
+      # create === new and save
+      newprofile = Profile.create(first_name: current_user.first_name,
+                                  last_name: current_user.last_name,
+                                  email: current_user.email,
+                                  user_holder_id: userholder.id)
+    end
+    @current_profile = userholder.profile
+  end
+
+
+  def getFullAddress(current_profile)
+    if checkIfFullAddressAvailable then
+      ( current_profile.address_line1 +
+        emptyField(current_profile.address_line2) ? "" : current_profile.address_line2 +
+        current_profile.city +
+        current_profile.state +
+        current_profile.country +
+        current_profile.postal_code )
+    else
+      "Missing some of the fields for address, click edit to update your profile."
+    end
+  end
+
+  def checkIfFullAddressAvailable
+    return !( emptyField?(@current_profile.address_line1) ||
+              emptyField?(@current_profile.city) ||
+              emptyField?(@current_profile.state) ||
+              emptyField?(@current_profile.country) ||
+              emptyField?(@current_profile.postal_code) )
+  end
+
+  def emptyField?(field)
+    field.nil? || field == ''
+  end
+
+end
