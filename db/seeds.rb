2e998e9b0c12c894ffd620becf8302d2e2e46c55
nathanielng2017 <stoneplus@berkeley.edu>
2019-11-04 11:09:20 -0800

    added seed for user userholdr and profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# users = [{:first_name => "Peter", :last_name => "Pei", :email => "pp@gmail.com", :encrypted_password => "123", :reset_password_token => "123"}]
-profiles= [{:first_name => 'Tom', :last_name => "Brady", :email => "1@gmai.com", :user_holder_id => 1},
-           {:first_name => 'Tom', :last_name => "James", :email => "2@gmai.com", :user_holder_id => 2},
-           {:first_name => 'Bill', :last_name => "Gates", :email => "3@gmai.com", :user_holder_id => 3},
-           {:first_name => 'Bill', :last_name => "Jobs", :email => "4@gmai.com", :user_holder_id => 4},
-           {:first_name => 'Steven', :last_name => "Jobs", :email => "5@gmai.com", :user_holder_id => 5},
-  	 ]
-# users.each do |user|
-#     User.create!(user)
-# end
+users = [{:first_name => "Peter", :last_name => "Pei", :email => "pp2@gmail.com",  :password => "password", :password_confirmation => "password"},
+         {:first_name => "Tom", :last_name => "Brady", :email => "tomb2@gmail.com",  :password => "password", :password_confirmation => "password"},
+         {:first_name => "Steven", :last_name => "Jobs", :email => "stevenj2@gmail.com",  :password => "password", :password_confirmation => "password"},
+         {:first_name => "Bill", :last_name => "Gates", :email => "bliig2@gmail.com",  :password => "password", :password_confirmation => "password"},
+]
 
-profiles.each do |profile|
-  Profile.create!(profile)
+users.each do |current_user|
+  new_user = User.new(first_name: current_user[:first_name], last_name: current_user[:last_name], email: current_user[:email], password: current_user[:password], password_confirmation: current_user[:password_confirmation])
+  # new_user.skip_confirmation!
+  new_user.save!
+  new_user_holder = UserHolder.create!(first_name: new_user.first_name,
+                                  last_name: new_user.last_name,
+                                  email: new_user.email,
+                                  user_id: new_user.id)
+  newprofile = Profile.create(first_name: new_user.first_name,
+                              last_name: new_user.last_name,
+                              email: new_user.email,
+                              user_holder_id: new_user_holder.id)
 end
