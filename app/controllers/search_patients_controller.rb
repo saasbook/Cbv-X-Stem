7df09f44e845b3f2f95e367c34b241b345a91a1b
PeterPei <pjspjspjs810@gmail.com>
2019-11-05 16:38:57 -0800

    searchPateints

--- app/controllers/search_patients_controller.rb
+++ app/controllers/search_patients_controller.rb
@@ -1,11 +1,9 @@
 class SearchPatientsController < ApplicationController
     def searchPatients
         @patients = Profile.all
-        puts(params[:searchPatients])
         if params[:searchPatients]
             @patients = Profile.where(first_name: params[:searchPatients]) + Profile.where(last_name: params[:searchPatients])
         end
-        puts(@patients.to_json)
     end
 
     def findResults
