8e5ec3a3e13cc78876d9474ff93ecaec1944687b
PeterPei <pjspjspjs810@gmail.com>
2019-11-05 16:38:57 -0800

    searchPateints

--- app/controllers/search_patients_controller.rb
+++ app/controllers/search_patients_controller.rb
@@ -1,14 +1,13 @@
 class SearchPatientsController < ApplicationController
     def searchPatients
         @patients = Profile.all
-        if (!params[:search_first_name].nil? && !params[:search_first_name].empty?) 
-            @patients = @patients.where('lower(first_name) = ? ', params[:search_first_name].downcase)
+        if !params[:searchPatients].nil? && !params[:searchPatients].empty? 
+            puts("hh")
+            @patients = Profile.where(first_name: params[:searchPatients]) + Profile.where(last_name: params[:searchPatients])
         end
-        if (!params[:search_last_name].nil? && !params[:search_last_name].empty?)
-            @patients = @patients.where('lower(last_name) = ? ', params[:search_last_name].downcase)
-        end
-        if (!params[:search_id].nil? && !params[:search_id].empty?)
-            @patients = @patients.where(:user_holder_id => params[:search_id])
-        end
+    end
+
+    def findResults
+        redirect_to searchPatients_path :searchPatients => params[:searchPatients]
     end
 end
