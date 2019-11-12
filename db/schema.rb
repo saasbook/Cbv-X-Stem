442452b0f6824511358224c29b72500d58ae8a00
Brian <bkevch@berkeley.edu>
2019-11-11 16:15:49 -0800

    added migration for boolean column is_doctor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_164632) do
+ActiveRecord::Schema.define(version: 2019_11_12_001246) do
 
   create_table "documentations", force: :cascade do |t|
     t.string "patient"
@@ -44,8 +44,6 @@ ActiveRecord::Schema.define(version: 2019_11_04_164632) do
     t.string "last_name"
     t.string "email"
     t.integer "user_holder_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "address_line1"
     t.string "address_line2"
     t.string "city"
@@ -67,7 +65,6 @@ ActiveRecord::Schema.define(version: 2019_11_04_164632) do
     t.string "current_job"
     t.string "exercise"
     t.string "doctor"
-    t.index ["user_holder_id"], name: "index_profiles_on_user_holder_id"
   end
 
   create_table "treatments", force: :cascade do |t|
@@ -106,6 +103,7 @@ ActiveRecord::Schema.define(version: 2019_11_04_164632) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_doctor"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
