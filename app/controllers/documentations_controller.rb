2dfc54d4c79b2911ee138156269b517c787bb8c0
Kunal Basu <kunal.basu@berkeley.edu>
2019-11-18 08:54:40 -0800

    Changes so far for Donwload Feature

--- app/controllers/documentations_controller.rb
+++ app/controllers/documentations_controller.rb
@@ -17,15 +17,26 @@ class DocumentationsController < ApplicationController
       end
   end
 
+
+  def download_pdf
+    @documentation = Documentation.find(params[:id])
+    puts @documentation.inspect
+    puts @documentation.attachment_url
+    puts "DOWNLOADING METHOD"
+    send_data("#{Rails.root}/public/" + @documentation.attachment_url, type: "application/pdf", x_sendfile: true)
+    # redirect_to documentations_path, notice: "The document #{@documentation.patient} has been downloaded."
+  end
+
   def destroy
     @documentation = Documentation.find(params[:id])
     @documentation.destroy
+    puts "TEST"
     redirect_to documentations_path, notice: "The document #{@documentation.patient} has been deleted."
   end
 
   private
     def documentation_params
-    params.require(:documentation).permit(:patient, :attachement)
+    params.require(:documentation).permit(:patient, :attachment)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'documentations/new'
   get 'documentations/create'
   get 'documentations/destroy'
+  get 'documentations/download_pdf'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
