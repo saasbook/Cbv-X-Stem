f8d4f7d6b89e660432e430d9c149965537af71cd
nathanielng2017 <stoneplus@berkeley.edu>
2019-11-18 00:28:17 -0800

    config 01

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   resources :documentations, only: [:index, :new, :create, :destroy]
   resources :messages
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
