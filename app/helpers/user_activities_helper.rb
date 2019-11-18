f5fdaacd0f4883341e5d80b61e3aad4085f4cfa6
nathanielng2017 <stoneplus@berkeley.edu>
2019-11-18 03:36:50 -0800

    added field for changed field and skip date update

--- app/helpers/user_activities_helper.rb
+++ app/helpers/user_activities_helper.rb
@@ -5,8 +5,8 @@ module UserActivitiesHelper
     actor = user_for_user_holder.first_name + " " + user_for_user_holder.last_name + (user_for_user_holder.is_doctor ? " (Doctor)": "")
     # byebug
     new_item.keys.each do |col_name|
-      if new_item[col_name] != previous_item[col_name]
-        user_for_user_holder.user_holder.user_activities.create(category: category, actor: actor, action: mode, original_val: previous_item[col_name], new_val: new_item[col_name])
+      if new_item[col_name] != previous_item[col_name] && col_name != "created_at" && col_name != "updated_at"
+        user_for_user_holder.user_holder.user_activities.create(category: category, actor: actor, action: mode, field: col_name, original_val: previous_item[col_name], new_val: new_item[col_name])
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_090304) do
+ActiveRecord::Schema.define(version: 2019_11_18_112916) do
 
   create_table "documentations", force: :cascade do |t|
     t.string "patient"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_090304) do
     t.integer "user_holder_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "field"
     t.index ["user_holder_id"], name: "index_user_activities_on_user_holder_id"
   end
 
