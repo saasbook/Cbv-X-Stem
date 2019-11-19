9b7eb1551c22996b12a71f5fc25a8b0572aa1814
Yiwen Chen <yiwen66@berkeley.edu>
2019-11-18 16:38:35 -0800

    change scheme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_165424) do
+ActiveRecord::Schema.define(version: 2019_11_18_224504) do
 
   create_table "documentations", force: :cascade do |t|
     t.string "patient"
