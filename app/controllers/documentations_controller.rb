a1ab5e21d34afc223bd221905ec492361ebb377f
Yiwen Chen <yiwen66@berkeley.edu>
2019-11-18 18:05:14 -0800

    integrate notification with setting

--- app/controllers/documentations_controller.rb
+++ app/controllers/documentations_controller.rb
@@ -8,22 +8,25 @@ class DocumentationsController < ApplicationController
   end
 
   def create
-    @documentation = Documentation.new(documentation_params)
+      @documentation = Documentation.new(documentation_params)
       
       if @documentation.save
+         
          redirect_to documentations_path, notice: "The document #{@documentation.patient} has been uploaded."
          @first_name, @last_name = @documentation.patient.split
          @cur_user = User.where(first_name: @first_name, last_name: @last_name).first
-         @cur_user_email = @cur_user.email
-         @message = Message.new(:sender_name => @documentation.patient)
-         @message.receiver_email = 'cbvxstem@gmail.com'
-         MessageMailer.document_notification(@message).deliver
-         if @cur_user_email != ""   
-          @message.sender_email =  @cur_user_email
-          MessageMailer.document_confirmation(@message).deliver
-         end
-        
-         
+         @current_holder = @cur_user.user_holder
+         @current_setting = @current_holder.user_setting
+          if @current_setting.email_notification
+            @cur_user_email = @cur_user.email
+            @message = Message.new(:sender_name => @documentation.patient)
+            @message.receiver_email = 'cbvxstem@gmail.com'
+            MessageMailer.document_notification(@message).deliver
+            if @cur_user_email != ""   
+              @message.sender_email =  @cur_user_email
+              MessageMailer.document_confirmation(@message).deliver
+            end
+          end           
       else
          render "new"
       end
