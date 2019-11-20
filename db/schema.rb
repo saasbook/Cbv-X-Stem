2bf8af11f896e97ae4cd7c75fa95ebbbdef7360f
Kaijing <kaijing@berkeley.edu>
2019-11-19 17:24:18 -0800

    .

--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,11 @@ ActiveRecord::Schema.define(version: 2019_11_19_102809) do
     t.string "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.text "documents_info"
     t.string "documents_status"
     t.string "documents_name"
     t.text "documents_explanation"
+    t.string "status"
   end
 
   create_table "documents", force: :cascade do |t|
@@ -75,8 +75,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_102809) do
     t.string "last_name"
     t.string "email"
     t.integer "user_holder_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "address_line1"
     t.string "address_line2"
     t.string "city"
@@ -99,7 +97,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_102809) do
     t.string "exercise"
     t.string "doctor"
     t.string "whatsapp"
-    t.index ["user_holder_id"], name: "index_profiles_on_user_holder_id"
   end
 
   create_table "treatments", force: :cascade do |t|
