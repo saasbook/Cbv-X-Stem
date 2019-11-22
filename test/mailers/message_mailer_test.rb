28021d38f9d02230694f642208b8651fffce8f9e
Kaijing <kaijing@berkeley.edu>
2019-11-22 14:47:00 -0800

    tring to add tests

--- test/mailers/message_mailer_test.rb
+++ test/mailers/message_mailer_test.rb
@@ -9,4 +9,17 @@ class MessageMailerTest < ActionMailer::TestCase
     # assert_match "Hi", mail.body.encoded
   end
 
+  test "clinic_message" do
+  	email = MessageMailer.clinic_message(messages(:clinic))
+
+  	assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['cbvxstem@gmail.com'], email.from
+    assert_equal ['cbvxstem@gmail.com'], email.to
+    assert_equal "New Message from Cbv X Stem", email.subject
+    assert_equal "MyText", email.body
+  end
+
 end
